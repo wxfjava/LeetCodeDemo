8ec0813c7a0cc3753a7b7a37e303be11a1b56040
wangxingfeng <wangxingfeng@tigerjoys.com>
2018-12-25 17:56:40 +0800

    动态规划

--- src/main/java/com/wxfjava/leetcode/solution1/Solution10.java
+++ src/main/java/com/wxfjava/leetcode/solution1/Solution10.java
@@ -139,6 +139,31 @@ public class Solution10 {
         }
     }
 
+    /**
+     * 动态规划
+     *
+     * @param s
+     * @param p
+     * @return
+     */
+    public boolean isMatch2(String s, String p) {
+        int sLen = s.length(), pLen = p.length();
+        boolean[][] memory = new boolean[sLen + 1][pLen + 1];
+        memory[0][0] = true;
+        for (int i = 0; i <= sLen; i++) {
+            for (int j = 1; j <= pLen; j++) {
+                if (p.charAt(j - 1) == '*') {
+                    memory[i][j] = memory[i][j - 2] || (i > 0 && (s.charAt(i - 1) == p.charAt(j - 2) ||
+                            p.charAt(j - 2) == '.') && memory[i - 1][j]);
+                } else {
+                    memory[i][j] = i > 0 && (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '.')
+                            && memory[i - 1][j - 1];
+                }
+            }
+        }
+        return memory[sLen][pLen];
+    }
+
     public static void main(String[] args) {
         Solution10 sol = new Solution10();
 
