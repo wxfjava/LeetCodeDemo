8dc93f532d299fdcacf766ae91f75c3aa0da5af0
wangxingfeng <wangxingfeng@tigerjoys.com>
2019-01-14 15:19:31 +0800

    12. 整数转罗马数字

--- src/main/java/com/wxfjava/leetcode/solution1/Solution11.java
+++ src/main/java/com/wxfjava/leetcode/solution1/Solution11.java
@@ -1,6 +1,7 @@
 package com.wxfjava.leetcode.solution1;
 
 /**
+ * 11. 盛最多水的容器
  * 给定 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点 (i, ai) 。在坐标内画 n 条垂直线，垂直线 i 的两个端点分别为 (i, ai) 和 (i, 0)。找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
  * <p>
  * 说明：你不能倾斜容器，且 n 的值至少为 2。
@@ -52,6 +53,25 @@ public class Solution11 {
         }
 
         return maxArea;
+    }
+
+    /**
+     * 双指针法
+     *
+     *
+     */
+    public class Solution {
+        public int maxArea(int[] height) {
+            int maxarea = 0, l = 0, r = height.length - 1;
+            while (l < r) {
+                maxarea = Math.max(maxarea, Math.min(height[l], height[r]) * (r - l));
+                if (height[l] < height[r])
+                    l++;
+                else
+                    r--;
+            }
+            return maxarea;
+        }
     }
 
     public static void main(String[] args) {
